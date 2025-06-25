9c023583d2da9c272f3bcf7930d3701edf46f8c2
BakhtiyorSharipov <bsharipov731@gmail.com>
2025-06-25 23:18:32 +0500

    Initial commit

---
+++ Domain/Model/CompanyEntity.cs
@@ -0,0 +1,10 @@
+namespace Domain.Model;
+
+public class CompanyEntity
+{
+    public int Id { get; set; }
+    public string? CompanyName { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? EmailAddress { get; set; }
+}
---
+++ Domain/Model/CustomerEntity.cs
@@ -0,0 +1,10 @@
+namespace Domain.Model;
+
+public class CustomerEntity
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Password { get; set; }
+}
