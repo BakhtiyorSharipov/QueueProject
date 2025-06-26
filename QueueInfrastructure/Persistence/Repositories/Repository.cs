128e0501cc824d99243d7291d35965a2721dd2c5
BakhtiyorSharipov <bsharipov731@gmail.com>
2025-06-26 13:19:41 +0500

    Implementing IRepository with empty methods

--- QueueInfrastructure/Persistence/Repositories/Repository.cs
+++ QueueInfrastructure/Persistence/Repositories/Repository.cs
@@ -1,6 +1,37 @@
+using Application.Common.Interfaces.Repository;
+using Domain.Model;
+
 namespace QueueInfrastructure.Persistence.Repositories;
 
-public class Repository
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
 {
-    
+    public TEntity FindById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TEntity> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int SaveChanges()
+    {
+        throw new NotImplementedException();
+    }
 }
